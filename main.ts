ba1bc7f8d1e08c011018f9b675aeb57b32406445
Nikolay Matrosov <nikthespirit@yandex-team.ru>
2023-01-03 22:15:33 +0100

    continue experimenting

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 import {create, Data, insert, load, save, search} from "@lyrasearch/lyra";
 import {stemmer} from "@lyrasearch/lyra/dist/cjs/stemmer/lib/ru.js";
+import {persistToFile, restoreFromFile} from '@lyrasearch/plugin-data-persistence'
 import {decode, encode} from "@msgpack/msgpack";
 import parse from 'fast-json-parse';
 import * as fs from "fs";
@@ -54,24 +55,29 @@ function serialize() {
     console.timeEnd('serialize');
 }
 
-function compressBrotli() {
-    console.time("brotli");
+function compressBrotli(q: number) {
+    const label = "brotli" + q
+    console.time(label);
     // Creating readable Stream
     const inp = fs.createReadStream(jsonFile);
 
     // Creating writable stream
-    const out = fs.createWriteStream(jsonFile + ".brotli");
+    const out = fs.createWriteStream(jsonFile + "." + label);
 
     // Calling createBrotliCompress method
-    const brot = zlib.createBrotliCompress();
+    const brot = zlib.createBrotliCompress({
+        params: {
+            [zlib.constants.BROTLI_PARAM_QUALITY]: q
+        }
+    });
 
-    brot.on("data", (chunk) => console.timeLog("brotli", chunk.length))
+    // brot.on("data", (chunk) => console.timeLog("brotli", chunk.length))
     // Piping
     return new Promise((resolve, reject) => {
         inp.pipe(brot)
             .pipe(out)
             .on("close", () => {
-                console.timeEnd("brotli");
+                console.timeEnd(label);
                 resolve({});
             })
             .on("error", (err) => {
@@ -81,23 +87,27 @@ function compressBrotli() {
     })
 }
 
-async function compressGzip() {
-    console.time("gz");
+async function compressGzip(q?: number) {
+    const quality = q ?? "";
+    const label = "gz" + quality
+    console.time(label);
     // Creating readable Stream
     const inp = fs.createReadStream(jsonFile);
 
     // Creating writable stream
-    const out = fs.createWriteStream(jsonFile + ".gz");
+    const out = fs.createWriteStream(jsonFile + "." + label);
 
     // Calling createGzip method
-    const gz = zlib.createGzip();
+    const gz = zlib.createGzip({
+        level: q ?? zlib.constants.Z_DEFAULT_COMPRESSION
+    });
 
     // Piping
     return new Promise((resolve, reject) => {
         inp.pipe(gz)
             .pipe(out)
             .on("close", () => {
-                console.timeEnd("gz");
+                console.timeEnd(label);
                 inp.close();
                 resolve({});
             })
@@ -124,10 +134,11 @@ async function serializeProto() {
     fs.writeFileSync(jsonFile + ".pts", data);
 }
 
-async function loadBrotli() {
-    console.time("loadBrotli");
+async function loadBrotli(q: number) {
+    const label = "loadBrotli" + q
+    console.time(label);
     const brotli = zlib.createBrotliDecompress();
-    const inp = fs.createReadStream(jsonFile + ".brotli");
+    const inp = fs.createReadStream(jsonFile + ".brotli" + q);
     inp.pipe(brotli);
     const chunks = []
     for await (let chunk of brotli) {
@@ -135,7 +146,7 @@ async function loadBrotli() {
     }
     const data = JSON.parse(Buffer.concat(chunks).toString())
     load(db, data);
-    console.timeEnd("loadBrotli");
+    console.timeEnd(label);
 }
 
 async function loadGzip() {
@@ -161,9 +172,9 @@ async function loadRaw() {
     // for await (let chunk of inp) {
     //     chunks.push(chunk)
     // }
-    console.time("parse")
+    // console.time("parse")
     const {value: data} = parse(fs.readFileSync(jsonFile))
-    console.timeEnd("parse")
+    // console.timeEnd("parse")
     load(db, data);
     console.timeEnd("loadRaw");
 }
@@ -205,6 +216,14 @@ async function serializeMessagePack() {
     console.timeEnd("serializeMessagePack");
 }
 
+function serializeNative(format: PersistenceFormat) {
+    const {value: json} = parse(fs.readFileSync(jsonFile))
+    load(db, json)
+    console.time("serializeNative" + format);
+    persistToFile(db, format, `./data/native.${format}`)
+    console.timeEnd("serializeNative" + format);
+}
+
 
 async function loadMessagePack() {
     console.time("loadMessagePack");
@@ -216,16 +235,52 @@ async function loadMessagePack() {
     console.timeEnd("loadMessagePack");
 }
 
-// await index();
-// serialize();
-// await compressBrotli();
-// await compressGzip();
-// await loadBrotli();
-await loadRaw();
-// await serializeProto()
-// await loadProto()
-// await loadGzip();
-console.log(search(db, {
+type PersistenceFormat = "binary" | "dpack" | "json";
+
+function loadNative(format: PersistenceFormat) {
+    console.time("loadNative" + format);
+    const restoredInstance = restoreFromFile(format, `./data/native.${format}`)
+    console.timeEnd("loadNative" + format);
+}
+
+await index();
+serialize();
+await compressBrotli(zlib.constants.BROTLI_MIN_QUALITY);
+await compressBrotli(zlib.constants.BROTLI_MAX_QUALITY);
+await compressGzip();
+await compressGzip(zlib.constants.Z_BEST_SPEED);
+await compressGzip(zlib.constants.Z_BEST_COMPRESSION);
+
+["binary", "dpack", "json"].forEach(f => {
+    serializeNative(f as PersistenceFormat);
+    loadNative(f as PersistenceFormat);
+})
+
+switch (process.argv[2]) {
+    case "raw": {
+        await loadRaw();
+        break;
+    }
+    case "brotli0": {
+        await loadBrotli(zlib.constants.BROTLI_MIN_QUALITY);
+        break;
+    }
+    case "brotli11": {
+        await loadBrotli(zlib.constants.BROTLI_MAX_QUALITY);
+        break;
+    }
+    case "mp": {
+        await loadMessagePack()
+        break;
+    }
+    case "gzip": {
+        await loadGzip();
+        break;
+    }
+}
+
+
+search(db, {
     term: "Иваныч",
     properties: ["joke"],
-}))
+})
